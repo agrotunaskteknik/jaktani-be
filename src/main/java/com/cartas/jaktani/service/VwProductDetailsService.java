90bfe3bedd593e92c44eb4e7cd48b139ef13cb37
muhib416 <muhibuddin.id@gmail.com>
2020-11-28 02:34:31 +0700

    Get product type list by product id

--- src/main/java/com/cartas/jaktani/service/VwProductDetailsService.java
+++ src/main/java/com/cartas/jaktani/service/VwProductDetailsService.java
@@ -4,6 +4,7 @@ import com.cartas.jaktani.dto.ParamRequestDto;
 
 public interface VwProductDetailsService {
 	Object findByProductId(Integer productId);
+	Object allProductTypeByProductId(Integer productId);
 	Object findAllByShopId(Integer shopId);
 	Object searchAllProduct(ParamRequestDto paramRequestDto);
 }
